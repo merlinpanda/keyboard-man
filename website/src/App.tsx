c469fa13df57140def766fc3cefce3b9cc1c0584
Salva Cheung <salva.cheung@outlook.com>
2023-02-21 20:01:30 +0800

    refactor(keyitem.tsx): adjust key item component

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react'
-import './App.css'
 import UsKeys from './utils/us-keys'
-import KeyItem from './components/KeyItem'
+import KeyItem, { PressKeyInterface } from './components/KeyItem'
+
+
+const WORDS = 'Youth is not a time of life; it is a state of mind; it is not a matter of rosy cheeks, red lips and supple knees; it is a matter of the will, a quality of the imagination, a vigor of the emotions; it is the freshness of the deep springs of life.';
 
 function App() {
   const KeyLine0 = ["Backquote","Digit1","Digit2","Digit3","Digit4","Digit5","Digit6","Digit7","Digit8","Digit9","Digit0","Minus","Equal","Backspace"]
@@ -10,63 +12,52 @@ function App() {
   const KeyLine3 = ["ShiftLeft","KeyZ","KeyX","KeyC","KeyV","KeyB","KeyN","KeyM","Comma","Period","Slash","ShiftRight"]
   const KeyLine4 = ["ControlLeft","MetaLeft","AltLeft","Space","AltRight","MetaRight","ContextMenu","ControlRight"]
 
-  const [ keypress, setKeypress ] = useState<string>('')
+  const [ keypress, setKeypress ] = useState<PressKeyInterface | undefined>(undefined)
   const [ timestamp, setTimeStamp ] = useState<number>(0)
+  const [ targetKeyCode, setTargetKeyCode ] = useState<number>(WORDS.charCodeAt(0))
+  const [ wordIndex, setWordIndex ] = useState<number>(0)
 
   const onKeyPress = (e: KeyboardEvent) => {
-    setKeypress('')
+    setKeypress(undefined)
     let code = e.code
     const charCode = e.key.charCodeAt(0)
     const isShift = e.shiftKey
-    setKeypress(code)
+    setKeypress({
+      code,
+      keyCode: charCode,
+      isShift,
+      key: e.key
+    })
     setTimeStamp((new Date()).getTime())
   }
 
   React.useEffect(() => {
     document.addEventListener('keypress', onKeyPress);
-    // const char = "[]\\;',./1234567890-=`";
-    // const uppers = '{}|:"<>?!@#$%^&*()_+~';
-    // // let keys = [];
-    // if (navigator.keyboard) {
-
-    //   const keyboard = '';
-    // }
-    // for(var i = 0; i < char.length; i ++) {
-    //   console.log(i,char[i],char.charCodeAt(i))
-    //   // let lowercode = char.charCodeAt(i);
-    //   // let uppercode = uppers.charCodeAt(i);
-      // let key = {
-      //   code: 'Key' + char[i].toUpperCase(),
-      //   key: char[i],
-      //   keyCode: lowercode,
-      //   shift: {
-      //     key: uppers[i],
-      //     keyCode: uppercode
-      //   },
-      //   showShift: false,
-      // }
-
-    //   // keys.push(key)
-    // }
-    // // console.log(keys)
-    // // console.log('68 char code', String.fromCharCode(68));
   }, [])
 
+  const onPressSuccess = () => {
+   setTimeout(() => {
+    setKeypress(undefined)
+    setWordIndex(wordIndex+1)
+    setTargetKeyCode(WORDS.charCodeAt(wordIndex))
+   }, 100)
+  }
+
   const KeyItemCom = (code: string) => {
-    let keyTarget = UsKeys.find(key => {
+    let keyItemData = UsKeys.find(key => {
       return key.code === code
     })
-    const isRight = keypress === code
-    let isActive = keypress == code
-    return keyTarget ? <KeyItem keyitem={keyTarget} isActive={isActive} isError={false} isRight={false} isTarget={false} timestamp={timestamp} /> : '';
+    return keyItemData ? <KeyItem onSuccess={() => {
+      onPressSuccess()
+    }} key={code} keyData={keyItemData} pressKey={keypress} targetKeyCode={targetKeyCode} timestamp={timestamp} /> : '';
   }
 
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <div className='p-2 text-2xl text-center mb-6'>Keyboard Man</div>
+      <div className='p-2 text-xl mb-6 max-w-4xl mx-auto'>{WORDS}</div>
       <div className="keyboard flex flex-col gap-1 justify-center">
         <div className="flex flex-row justify-center gap-1">
-          {KeyLine0.map(key => {
+          {KeyLine0.map((key) => {
             return KeyItemCom(key)
           })}
         </div>
--- website/src/components/KeyItem.tsx
+++ website/src/components/KeyItem.tsx
@@ -1,19 +1,15 @@
 import React, { RefObject } from 'react'
 import classnames from 'classnames'
 
-export interface KeyItemInterface {
+export interface PressKeyInterface {
   code: string;
   key: string;
   keyCode: number;
-  shift?: {
-    key: string;
-    keyCode: number;
-  };
-  showShift: boolean;
+  isShift: boolean;
 }
 
 interface KeyItemProps {
-  keyitem: {
+  keyData: {
     code: string;
     key: string;
     keyCode: number;
@@ -24,17 +20,13 @@ interface KeyItemProps {
     showShift: boolean;
     flexWidth?: number;
   };
-  isActive: boolean;
-  isRight: boolean;
-  isError: boolean;
-  isTarget: boolean;
   timestamp: number;
+  targetKeyCode: number;
+  pressKey?: PressKeyInterface,
+  onSuccess: Function
 }
 
 class KeyItem extends React.Component<KeyItemProps, any> {
-
-  public isActive: boolean = false
-
   public keyDom: RefObject<HTMLDivElement> | undefined
 
   constructor(props: KeyItemProps) {
@@ -43,33 +35,109 @@ class KeyItem extends React.Component<KeyItemProps, any> {
     this.keyDom = React.createRef<HTMLDivElement>()
   }
 
+  /**
+   * PressKey == KeyData
+   */
+  get isActive(): boolean {
+    const { keyData, pressKey } = this.props
+    if (pressKey?.isShift) {
+      return (
+        keyData.shift?.key === pressKey.key
+        && keyData.shift.keyCode === pressKey.keyCode
+        && keyData.code === pressKey.code
+      )
+    } else {
+      return (
+        keyData.key === pressKey?.key
+        && keyData.keyCode === pressKey.keyCode
+        && keyData.code === pressKey.code
+      )
+    }
+  }
+
+  /**
+   * PressKey == KeyData , and PressKey == Target
+   */
+  get isRight(): boolean {
+    const { keyData, pressKey, targetKeyCode } = this.props
+    if (pressKey?.isShift) {
+      return (
+        keyData.shift?.key === pressKey.key
+        && keyData.shift.keyCode === pressKey.keyCode
+        && keyData.code === pressKey.code
+        && pressKey.keyCode === targetKeyCode
+      )
+    } else {
+      return (
+        keyData.key === pressKey?.key
+        && keyData.keyCode === pressKey.keyCode
+        && keyData.code === pressKey.code
+        && pressKey.keyCode === targetKeyCode
+      )
+    }
+  }
+
+  /**
+   * PressKey == KeyData , but PressKey != Target
+   */
+  get isError(): boolean {
+    const { keyData, pressKey, targetKeyCode } = this.props
+    if (pressKey?.isShift) {
+      return (
+        keyData.shift?.key === pressKey.key
+        && keyData.shift.keyCode === pressKey.keyCode
+        && keyData.code === pressKey.code
+        && pressKey.keyCode !== targetKeyCode
+      )
+    } else {
+      return (
+        keyData.key === pressKey?.key
+        && keyData.keyCode === pressKey.keyCode
+        && keyData.code === pressKey.code
+        && pressKey.keyCode !== targetKeyCode
+      )
+    }
+  }
+
+  /**
+   * KeyData == Target
+   */
+  get isTarget(): boolean {
+    const { keyData, targetKeyCode } = this.props
+    return keyData.keyCode === targetKeyCode || keyData.shift?.keyCode === targetKeyCode;
+  }
+
   componentDidUpdate(): void {
-    if (this.props.isActive) {
+    if (this.isActive) {
       this.keyDom?.current?.classList.remove('key-active')
       window.requestAnimationFrame(() => {
         this.keyDom?.current?.classList.add('key-active')
       })
     }
+    if (this.isRight) {
+      this.props.onSuccess()
+    }
   }
 
   render() {
-    const { keyitem, isActive, isError, isTarget } = this.props
+    const { keyData } = this.props
+    const { isActive, isRight, isError, isTarget } = this
 
     return (
       <>
         <div ref={this.keyDom} className={classnames({
           key: true,
           'key-target': isTarget,
-          'key-right': isActive,
-          'key-error': !isActive && isError,
-          'key-pointer': keyitem.code == 'KeyF' || keyitem.code == 'KeyJ'
+          'key-right': isActive && isRight,
+          'key-error': isActive && isError,
+          'key-pointer': keyData.code == 'KeyF' || keyData.code == 'KeyJ'
         })} style={{
-          flex: keyitem?.flexWidth || 1,
+          flex: keyData?.flexWidth || 1,
         }}>
-          {keyitem.showShift && (
-            <div>{keyitem.shift?.key}</div>
+          {keyData.showShift && (
+            <div>{keyData.shift?.key}</div>
           )}
-          <div>{keyitem.key.toUpperCase()}</div>
+          <div>{keyData.key.toUpperCase()}</div>
         </div>
       </>
     )
